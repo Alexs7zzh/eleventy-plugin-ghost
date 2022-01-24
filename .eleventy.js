59e8cf6e17be51d7817118bf0a3e76c5e5ce6635
Alex Zhou <alex.zzh.s7@gmail.com>
2022-01-24 21:12:04 +0900

    init

--- .eleventy.js
+++ .eleventy.js
@@ -1,97 +1,62 @@
 const Cache = require("@11ty/eleventy-cache-assets");
 
 // Get all post data
-const getPosts = async ({ url, key, version = "v4" }) => {
+const getPosts = async ({ url, key, cache }) => {
   const data = await Cache(
-    `${url}/ghost/api/${version}/content/posts/?key=${key}&limit=all&include=tags,authors`,
-    { duration: "1m", type: "json" }
+    `${url}/ghost/api/v4/content/posts/?key=${key}&limit=all&include=tags,authors`,
+    { duration: cache ? "1m" : "0s", type: "json" }
   );
 
-  const updatedPosts = await data.posts.map((post) => {
-    // Filter tags to just show public tags
-    // Reassign internal tags to an internalTags key
-    const publicTags = post.tags.filter((tag) => tag.visibility !== "internal");
-    const internalTags = post.tags.filter(
-      (tag) => tag.visibility === "internal"
-    );
-
-    return {
-      ...post,
-      tags: publicTags,
-      internalTags: internalTags,
-    };
-  });
-
-  return updatedPosts;
+  return data.posts;
 };
 
 // Get all page data
-const getPages = async ({ url, key, version = "v4" }) => {
+const getPages = async ({ url, key, cache }) => {
   const data = await Cache(
-    `${url}/ghost/api/${version}/content/pages/?key=${key}&limit=all&include=tags,authors`,
-    { duration: "1m", type: "json" }
+    `${url}/ghost/api/v4/content/pages/?key=${key}&limit=all&include=tags,authors`,
+    { duration: cache ? "1m" : "0s", type: "json" }
   );
-
-  const updatedPages = await data.pages.map((page) => {
-    // Filter tags to just show public tags
-    // Reassign internal tags to an internalTags key
-    const publicTags = page.tags.filter((tag) => tag.visibility !== "internal");
-    const internalTags = page.tags.filter(
-      (tag) => tag.visibility === "internal"
-    );
-
-    return {
-      ...page,
-      tags: publicTags,
-      internalTags: internalTags,
-    };
-  });
-
-  return updatedPages;
 
   return data.pages;
 };
 
 // Get all tag data
-const getTags = async ({ url, key, version = "v4" }) => {
+const getTags = async ({ url, key, cache }) => {
   const data = await Cache(
-    `${url}/ghost/api/${version}/content/tags/?key=${key}&limit=all&include=count.posts&filter=visibility:public`,
-    { duration: "1m", type: "json" }
+    `${url}/ghost/api/v4/content/tags/?key=${key}&limit=all&include=count.posts`,
+    { duration: cache ? "1m" : "0s", type: "json" }
   );
 
   return data.tags;
 };
 
 // Get all author data
-const getAuthors = async ({ url, key, version = "v4" }) => {
+const getAuthors = async ({ url, key, cache }) => {
   const data = await Cache(
-    `${url}/ghost/api/${version}/content/authors/?key=${key}&limit=all&include=count.posts`,
-    { duration: "1m", type: "json" }
+    `${url}/ghost/api/v4/content/authors/?key=${key}&limit=all&include=count.posts`,
+    { duration: cache ? "1m" : "0s", type: "json" }
   );
 
   return data.authors;
 };
 
 // Get all settings data
-const getSettings = async ({ url, key, version = "v4" }) => {
+const getSettings = async ({ url, key, cache }) => {
   const data = await Cache(
-    `${url}/ghost/api/${version}/content/settings/?key=${key}`,
-    {
-      duration: "1m",
-      type: "json",
-    }
+    `${url}/ghost/api/v4/content/settings/?key=${key}`,
+    { duration: cache ? "1m" : "0s", type: "json" }
   );
 
   return data.settings;
 };
 
-const getContent = async (params) => {
+const getContent = async (options) => {
   return {
-    posts: await getPosts(params),
-    pages: await getPages(params),
-    tags: await getTags(params),
-    authors: await getAuthors(params),
-    settings: await getSettings(params),
+    posts: await getPosts(options),
+    pages: await getPages(options),
+    tags: await getTags(options),
+    authors: await getAuthors(options),
+    settings: await getSettings(options),
   };
 };
 
@@ -100,25 +65,8 @@ module.exports = (eleventyConfig, options) => {
     if (!options.url || !options.key) {
       console.log("Invalid Ghost API key or URL");
     }
+    options.cache = options.cache !== undefined ? options.cache : true;
 
     return await getContent(options);
-  });
-
-  eleventyConfig.addFilter("filterPosts", (posts, key, value) => {
-    // Check for exclamation before the second parameter…
-    if (value.startsWith("!")) {
-      // Snip off that exclamation
-      const unprefixedValue = value.substring(1);
-
-      // Filter posts that don't include this value
-      return posts.filter((post) =>
-        post[key].every((item) => item.slug !== unprefixedValue)
-      );
-    }
-
-    // Filter posts that have the value
-    return posts.filter((post) =>
-      post[key].some((item) => item.slug === value)
-    );
   });
 };
